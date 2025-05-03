3ef92ba50d5c2dbf9b154362249a74ad7d959da4
jaewon <wgikimi11@gmail.com>
2025-05-03 23:43:40 +0900

    refactor: Tamagui 기반 컴포넌트 사용을 위한 App.js 구조 정리

--- App.js
+++ App.js
@@ -3,27 +3,31 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { registerRootComponent } from 'expo';
 import React from 'react';
 
+// tamagui import 추가
+import { TamaguiProvider } from 'tamagui';
+import tamaguiConfig from './tamagui.config';
+
 import AddChildScreen from './src/screens/AddChildScreen';
 import ChildDetailScreen from './src/screens/ChildDetailScreen';
 import ChildListScreen from './src/screens/ChildListScreen';
 import LoginScreen from './src/screens/LoginScreen';
 import SignupScreen from './src/screens/SignupScreen';
 
-
 const Stack = createNativeStackNavigator();
 
 function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Login" component={LoginScreen} />
-        <Stack.Screen name="Signup" component={SignupScreen} />
-        <Stack.Screen name="ChildList" component={ChildListScreen} />
-        <Stack.Screen name="AddChild" component={AddChildScreen} />
-        <Stack.Screen name="ChildDetail" component={ChildDetailScreen} />
-
-      </Stack.Navigator>
-    </NavigationContainer>
+    <TamaguiProvider config={tamaguiConfig}>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Login" component={LoginScreen} />
+          <Stack.Screen name="Signup" component={SignupScreen} />
+          <Stack.Screen name="ChildList" component={ChildListScreen} />
+          <Stack.Screen name="AddChild" component={AddChildScreen} />
+          <Stack.Screen name="ChildDetail" component={ChildDetailScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </TamaguiProvider>
   );
 }
 
